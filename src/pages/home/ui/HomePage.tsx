8753f12fa572df505cb47328df21bf99557b6e8d
Alena Baikova <baikovaa19@gmail.com>
2026-07-01 11:48:30 +0500

    feat: load the font

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -3,7 +3,7 @@ import './HomePage.scss';
 export const HomePage = () => {
   return (
     <main className="home-page">
-      <h1>EIS24</h1>
+      <h1 className="home-page__title">EIS24</h1>
     </main>
   );
 };
