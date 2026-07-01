99c179b44eccc1796e7df4c98b500fca4d4797c5
Alena Baikova <baikovaa19@gmail.com>
2026-07-01 12:07:06 +0500

    feat: add images

---
+++ src/shared/images/index.ts
@@ -0,0 +1,5 @@
+export { TrashIcon } from './ui/TrashIcon';
+export { default as iconColdWater } from './icon-cold-water.svg';
+export { default as iconElectricity } from './icon-electricity.svg';
+export { default as iconHeating } from './icon-heating.svg';
+export { default as iconHotWater } from './icon-hot-water.svg';
